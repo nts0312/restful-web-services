87dde9e4d0ccffda11a8115ac3a9f79984428a13
nitinsinghm111 <nitinsinghm111@gmail.com>
2023-03-30 04:34:26 +0530

    added UserException lastly and started a restful api

---
+++ src/main/java/com/ntsProject/restfulwebservices/user/UserResource.java
@@ -0,0 +1,50 @@
+package com.ntsProject.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService userDaoService;
+
+	@GetMapping("users")
+	public List<User> retrieveAllUsers() {
+		return userDaoService.findAll();
+	}
+
+	@GetMapping("users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+
+		User user = userDaoService.findOne(id);
+
+		if (user == null)
+			throw new UserNotFoundException("id:" + id);
+
+		return user;
+
+	}
+
+	@PostMapping("users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+//for also getting added user url 
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		// in order to get right response status for req like -> like for post 201
+		return ResponseEntity.created(location).build();
+
+	}
+
+}
